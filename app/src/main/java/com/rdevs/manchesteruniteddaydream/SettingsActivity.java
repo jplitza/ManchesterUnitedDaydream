a786feb98fc3029798c36e7ec7af74627ec71418
Rounak Khandeparkar <rounak@slingapp.in>
2017-04-21 17:11:33 +0530

    - tap to open daydream settings - basic ui - string,color changes

--- app/src/main/java/com/rdevs/manchesteruniteddaydream/SettingsActivity.java
+++ app/src/main/java/com/rdevs/manchesteruniteddaydream/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.rdevs.manchesteruniteddaydream;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 
 public class SettingsActivity extends AppCompatActivity  {
@@ -10,5 +13,12 @@ public class SettingsActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        findViewById(R.id.btnSettings).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Settings.ACTION_DREAM_SETTINGS));
+            }
+        });
     }
 }
